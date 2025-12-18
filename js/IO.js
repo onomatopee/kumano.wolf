a38a426ee481a82a76cb163ff8538aa2215a7587
Shinnosuke Shoda <raindrops@vivaldi.net>
2025-12-19 04:05:25 +0900

    v0.01

---
+++ js/IO.js
@@ -0,0 +1,80 @@
+let playGround = document.getElementById("playGround");
+
+function playGroundWrite(str) {
+  playGround.innerHTML = str; 
+}
+
+function gameStart() {
+  const str = `<h2>人数入力</h2>
+      <h3>フリーライダー陣営（FR）</h3>
+      <p>フリーライダー……人狼。提起者にはなれない</p><input type="number" id="fr" value="0">
+      <p>SN……狂人。採決拒否を提起されると死ぬ</p><input type="number" id="sn" value="0">
+      <br>
+
+      <h3>FRアンチ陣営（SC）</h3>
+      <p>SC長……占い師</p><input type="number" id="scboss" value="0">
+      <p>デスドラ……霊媒師。夜をひとつ消せる</p><input type="number" id="death" value="0">
+      <p>JK部長……騎士</p><input type="number" id="jk" value="0">
+      <p>部長委員長……市民。役職の重複はない</p><input type="number" id="bucho" value="0">
+      <p>B3……お香を焚いて誰かのはたらきを2日止められる</p><input type="number" id="b3" value="0">
+      <p>議長団……採決拒否を提起できる</p><input type="number" id="chair" value="0">
+      <br>
+      <button onclick="getAttr();nameInput()">入力</button>
+  `;
+  playGroundWrite(str);
+}
+
+function getAttr() {
+  attrNum["フリーライダー"] = document.getElementById("fr").value;
+  attrNum["SN"] = document.getElementById("sn").value;
+
+  attrNum["SC長"] = document.getElementById("scboss").value;
+  attrNum["デスドラ"] = document.getElementById("death").value;
+  attrNum["JK部長"] = document.getElementById("jk").value;
+  attrNum["部長委員長"] = document.getElementById("bucho").value;
+  attrNum["B3"] = document.getElementById("b3").value;
+  attrNum["議長団"] = document.getElementById("chair").value;
+
+  for (let i = 0; i < attrNum["フリーライダー"]; i++) attrList.push("フリーライダー");
+  for (let i = 0; i < attrNum["SN"]; i++) attrList.push("SN");
+  
+  for (let i = 0; i < attrNum["SC長"]; i++) attrList.push("SC長");
+  for (let i = 0; i < attrNum["デスドラ"]; i++) attrList.push("デスドラ");
+  for (let i = 0; i < attrNum["JK部長"]; i++) attrList.push("JK部長");
+  for (let i = 0; i < attrNum["部長委員長"]; i++) attrList.push("部長委員長");
+  for (let i = 0; i < attrNum["B3"]; i++) attrList.push("B3");
+  for (let i = 0; i < attrNum["議長団"]; i++) attrList.push("議長団");
+
+  shuffle(attrList);
+  console.log(attrNum);
+}
+
+function nameInput() {
+  console.log(attrList.length);
+  const ryoseiNum = attrList.length;
+  let str = "<h2>名前を入力</h2>";
+  for (let i = 0; i < ryoseiNum; i++) {
+    str += `<input type="text" id="name${i}"><br>`;
+  }
+  str += `<button onclick=getName();discussion()>第1議題へ</button>`
+  playGroundWrite(str);
+}
+
+function getName() {
+  const ryoseiNum = attrList.length;
+  for (let i = 0; i < ryoseiNum; i++) {
+    const name = document.getElementById(`name${i}`);
+    nameList.push(name);
+  }
+  generateRyosei();
+}
+
+function discussion() {
+  const claimer = chooseClaimer();
+  const claimerName = nameList[claimer];
+
+}
+
+function rest() {
+
+}
---
+++ js/system.js
@@ -0,0 +1,19 @@
+let aliveList =  [];
+let nameList = [];
+let attrList = [];
+let attrNum = {};
+
+function generateRyosei() {
+  const ryoseiNum = attrList.length();
+  for (let i = 0; i < ryoseiNum; i++) {
+    aliveList.push(true);
+  }
+}
+
+function chooseClaimer() {
+  ok = false
+  while (!ok) {
+    const candidate = Math.floor(Math.random() * (i + 1));
+    
+  }
+}
